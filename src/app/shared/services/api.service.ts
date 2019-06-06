10da901c0d3182a892613e40b17a920dee69d7a7
adrian <adrianfj@gmail.com>
2019-06-06 11:34:43 -0400

    Add reservation to api.service and connect to component

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -56,7 +56,7 @@ export class MainService {
   providedIn: 'root'
 })
 export class ReservationService {
-  API_URL = environment.devUrl + 'volt_reservation/';
+  API_URL = this.API_URL + 'volt_reservation/reservations/';
   user: User;
 
   constructor(private http: HttpClient) {
@@ -74,7 +74,7 @@ export class ReservationService {
   }
 
   public makeReservation(eventCsNk, evNk): Observable<Reservation> {
-    return this.http.post<Reservation>(this.API_URL + 'reservations/',
+    return this.http.post<Reservation>(this.API_URL,
       {
         event_cs_nk: eventCsNk,
         ev_nk: evNk
