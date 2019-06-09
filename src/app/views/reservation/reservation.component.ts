ec3b1c1719b1b0f6f1854a707c08b2adae1d3ad5
Niroigen Suntharam <niroigen@gmail.com>
2019-06-08 21:24:18 -0400

    See information of CS

--- src/app/views/reservation/reservation.component.ts
+++ src/app/views/reservation/reservation.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { ChargingStation } from '../../shared/models/charging-station';
 import { EventCS } from '../../shared/models/event-cs';
-import { ReservationService } from '../../shared/services/api.service';
+import { ReservationService, MainService } from '../../shared/services/api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reservation',
@@ -10,26 +11,42 @@ import { ReservationService } from '../../shared/services/api.service';
 })
 export class ReservationComponent implements OnInit {
 
-  eventCs: EventCS;
+  eventCss: EventCS[];
   evNk: string;
+  csNk: string;
   chargingStation: ChargingStation;
+  dataLoaded: Promise<boolean>;
 
   isReserved = false;
 
   constructor(
-    private reservationService: ReservationService
-  ) { }
+    private reservationService: ReservationService, private route: ActivatedRoute, private mainService: MainService
+  ) { this.route.params.subscribe( params => this.csNk = params.nk ); }
 
   ngOnInit() {
+    this.mainService.getChargingStation(this.csNk).subscribe(
+      chargingStation => {
+        this.chargingStation = chargingStation;
+        console.log(chargingStation);
+        this.dataLoaded = Promise.resolve(true);
+      }
+    );
+
+    this.reservationService.getAvailabilities('2019-09-25 12:00:00', '2019-09-28 15:30:00', this.csNk).subscribe(
+      eventCss => {
+        this.eventCss = eventCss;
+      }
+    );
+
     // Get CS info and availability
   }
 
-  public makeReservation() {
-    this.reservationService.makeReservation(this.evNk, this.eventCs.nk)
-      .subscribe(() => {
-        this.isReserved = true; // TODO replace by toasterNotification
-      }, (error) => {
-        console.error(error);
-      });
-  }
+  // public makeReservation() {
+  //   this.reservationService.makeReservation(this.evNk, this.eventCs.nk)
+  //     .subscribe(() => {
+  //       this.isReserved = true; // TODO replace by toasterNotification
+  //     }, (error) => {
+  //       console.error(error);
+  //     });
+  // }
 }
