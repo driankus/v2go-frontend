f1cff0eeb7617ece1d2f465f551d9ef36de77fe5
Niroigen Suntharam <niroigen@gmail.com>
2019-06-10 03:56:20 -0400

    Able to display calendar of CS Events

--- src/app/views/driver/driver.module.ts
+++ src/app/views/driver/driver.module.ts
@@ -9,6 +9,10 @@ import { DriverRoutingModule } from './driver-routing.module';
 import { HomeMapComponent } from '../home-map/home-map.component';
 import { StationDetailComponent } from '../home-map/station-detail/station-detail.component';
 import { ReservationComponent } from '../reservation/reservation.component';
+import { CalendarModule, CalendarUtils, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarFormDialogComponent } from '../calendar/calendar-form-dialog/calendar-form-dialog.component';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
     imports: [
@@ -18,11 +22,18 @@ import { ReservationComponent } from '../reservation/reservation.component';
       FormsModule,
       ReactiveFormsModule,
       NgbModule,
+      ColorPickerModule,
+      CalendarModule.forRoot({
+        provide: DateAdapter,
+        useFactory: adapterFactory
+      }),
       AgmCoreModule.forRoot({
         apiKey: environment.GOOGLE_API_KEY
       }),
       DriverRoutingModule
     ],
-    declarations: [ HomeMapComponent, StationDetailComponent, ReservationComponent ]
+    providers: [CalendarUtils],
+    declarations: [ HomeMapComponent, StationDetailComponent, ReservationComponent, CalendarFormDialogComponent ],
+    entryComponents: [CalendarFormDialogComponent]
   })
 export class DriverModule { }
