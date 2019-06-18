9fefa80524df3f216c4b88b800db36d93e22520a
adrian <adrianfj@gmail.com>
2019-06-18 11:41:15 -0400

    Create service to fetch user data

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -5,9 +5,7 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { ChargingStation } from '../models/charging-station';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class SearchStationsService {
   private API_URL = environment.devUrl + 'volt_finder/near-poi';
   constructor(
@@ -15,7 +13,7 @@ export class SearchStationsService {
   ) { }
 
   /**
-   * Performs CS search by calling the 'API/near-poi' endpoint.
+   * Search CS by calling the 'API/near-poi' endpoint.
    *
    * @param poiLat - Point of interest latitude, either from user's location or default lat (MTL)
    * @param poiLng - Point of interest longitude or default lng (MTL)
@@ -27,9 +25,42 @@ export class SearchStationsService {
       const params = new HttpParams()
         .set('poi_lat', String(poiLat))
         .set('poi_lng', String(poiLng));
-      return this.http.get<ChargingStation[]>(this.API_URL, { params: params }).pipe(
-        map(stationsList => stationsList.map(station => ChargingStation.create(station)))
-      );
+      return this.http.get<ChargingStation[]>(
+        this.API_URL, { params: params }).pipe(
+          map(stationsList => stationsList.map(
+            station => ChargingStation.create(station)
+      )));
     }
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+export class UserAccountInfoService {
+  private API_URL = environment.devUrl + 'my-account';
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  /**
+   * Get user personal info, EV, reservations, history, etc
+   * @param user_pk
+   */
+  getAccountInfo(user_pk: number): Observable<any> {
+    // TODO: Update API endpoint to take user_pk as param, meaning:
+    // 'my-account?user_pk=3', instead of 'my-account/3"
+    // const params = new HttpParams().set('user_pk', String(user_pk));
+    // return this.http.get<any>(
+    //   this.API_URL, { params: params }).pipe(
+    //     map(userData => userData)
+    //     // map(resp => resp.map(
+    //     //   userInfo => ChargingStation.create(station)))
+    //   );
+
+    const modifAPI = this.API_URL + '/' + String(user_pk);
+    return this.http.get<any>( modifAPI ).pipe(
+        map(userData => userData)
+        // map(resp => resp.map(
+        //   userInfo => ChargingStation.create(station)))
+      );
   }
 }
--- src/app/views/driver/profile/profile.component.ts
+++ src/app/views/driver/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserAccountInfoService } from '../../../shared/services/api.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  accountInfo: any;
+  user_pk = 3;
 
-  constructor() { }
+  constructor(
+    private accountService: UserAccountInfoService,
+    // private toastr: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.getUserInfo();
+  }
+
+  getUserInfo(): void {
+    this.accountService.getAccountInfo(this.user_pk)
+      .subscribe(userData => {
+        this.accountInfo = userData;
+        console.log('#'.repeat(100), ' #userData!!!: ', userData);
+      });
   }
 
 }
+
