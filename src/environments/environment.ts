ca03d8713977dc1a391a149053e4ca06bb3fd028
adrian <adrianfj@gmail.com>
2019-06-28 13:10:44 -0400

    Update environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  GOOGLE_API_KEY: 'AIzaSyBd8MecCOrabvEIDILrd5KLIyIKuJzhE3M',
+  GOOGLE_API_KEY: 'AIzaSyDn_yRSRRpEzuyW2X1hS4hlc7Rhdw9l5w0',
   API_URL: 'http://localhost:8888/api/v1.0-pre-alpha/'
   // API_URL: 'http://localhost:1337/api/v1.0-pre-alpha/'
 };
